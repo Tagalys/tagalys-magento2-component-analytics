af4a44bf61ee166a50ebafaa19a7387c81fdbe28
Palaniappan Chellappan <pc@tagalys.com>
2018-01-16 00:17:44 +0530

    initial commit

---
+++ Block/Productview.php
@@ -0,0 +1,34 @@
+<?php
+namespace Tagalys\Analytics\Block;
+ 
+class Productview extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(
+        \Tagalys\Sync\Helper\Configuration $tagalysConfiguration,
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Registry $registry
+    )
+    {
+        $this->tagalysConfiguration = $tagalysConfiguration;
+        $this->storeManager = $storeManager;
+        $this->registry = $registry;
+        parent::__construct($context);
+    }
+
+    public function isTagalysEnabled() {
+        return $this->tagalysConfiguration->isTagalysEnabledForStore($this->getCurrentStoreId());
+    }
+
+    public function getCurrentStoreId() {
+        return $this->storeManager->getStore()->getId();
+    }
+
+    public function getMainProduct() {
+        $mainProduct = $this->registry->registry('product');
+        if (is_object($mainProduct)) {
+          return $mainProduct;
+        }
+        return false;
+    }
+}
---
+++ Block/Track.php
@@ -0,0 +1,50 @@
+<?php
+namespace Tagalys\Analytics\Block;
+ 
+class Track extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(
+        \Tagalys\Analytics\CookieManager $cookieManager,
+        \Tagalys\Sync\Helper\Configuration $tagalysConfiguration,
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Url $frontUrlHelper,
+        \Magento\Framework\Registry $registry
+    )
+    {
+        $this->cookieManager = $cookieManager;
+        $this->tagalysConfiguration = $tagalysConfiguration;
+        $this->storeManager = $storeManager;
+        $this->registry = $registry;
+        $this->frontUrlHelper = $frontUrlHelper;
+        parent::__construct($context);
+    }
+
+    public function isTagalysEnabled() {
+        return $this->tagalysConfiguration->isTagalysEnabledForStore($this->getCurrentStoreId());
+    }
+
+    public function getCurrentStoreId() {
+        return $this->storeManager->getStore()->getId();
+    }
+
+    public function getEvent() {
+        try {
+            $event = $this->cookieManager->get('__ta_event');
+            $this->cookieManager->delete('__ta_event');
+            if ($event == false) {
+                $event = $this->registry->registry('tagalys_analytics_event');
+            }
+            if ($event != false) {
+                $event = json_decode($event, true);
+            }
+            return $event;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public function getProductDetailsUrl() {
+        return $this->frontUrlHelper->getUrl('tanalytics/product/details/');
+    }
+}
